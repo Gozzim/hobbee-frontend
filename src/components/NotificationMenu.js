5b4e660332f41f2ee3ea374c75c84720fe8ba0e4
Kempec Halk <kempec.halk@tum.de>
2021-07-02 00:33:16 +0200

    feat(Notification/Menu): Notification Items Draft

--- src/components/NotificationMenu.js
+++ src/components/NotificationMenu.js
@@ -1,101 +1,95 @@
-import React, {useEffect} from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
-import { Menu, MenuItem, Divider } from "@material-ui/core";
+import {
+  Menu,
+  Divider,
+  ListItem,
+  Typography,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
 import { connect } from "react-redux";
-import { fetchNotifications } from "../redux/reducers/notificationReducer";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+/*
+ * TODO:
+ *  - Map group to name
+ *  - Mark as read button functionality
+ *  - Mark all as read button functionality
+ *  - Notification Type indicator
+ *  - Notification Routing
+ *  - Filtering by read and unread
+ */
 
 const NotificationTypes = {
-    CHAT : "Chat",
-    REMINDER : "Reminder",
-    FEEDBACK : "Feedback"
-}
-
-const useStyles = makeStyles((theme) => ({
-    menuitem: {
-        display: "flex",
-        minWidth: "200px",
-    },
-}));
+  CHAT: "Chat",
+  REMINDER: "Reminder",
+  FEEDBACK: "Feedback",
+};
 
 /**
  * Menu for user managment
  * @param {props} props
  */
 function NotificationMenu(props) {
-    const classes = useStyles();
 
-    useEffect(() => {
-        props.dispatch(fetchNotifications())
-    });
+  const onClickNotification = (notificationType) => { //TODO
+    props.onClose();
 
-    let unread = () => this.props.notifications.filter(
-        (userNotification) => userNotification.unread
-    );
-
-    const onClickNotification = (notificationType) => {
-        props.onClose();
-
-        switch (notificationType) {
-            case NotificationTypes.CHAT:
-            case NotificationTypes.REMINDER:
-                let groupId = 0; // dummy
-                props.history.push("/group-page/" + groupId);
-                break;
-            case NotificationTypes.FEEDBACK:
-                /*
-                startFeedback(feedbackHash);
-                 */
-                break;
-            default:
-                break;
-        }
-    };
-
-    function renderNotificationItems(notifications) {
-        if (notifications.length === 0) {
-            return (
-                <div>
-                    <p>You have no new notifications.</p>
-                </div>
-            )
-        }
-        return (
-            <MenuItem
-                key="notification"
-                className={classes.menuitem}
-            >
-
-            </MenuItem>
-        );
+    switch (notificationType) {
+      case NotificationTypes.CHAT:
+      case NotificationTypes.REMINDER:
+        let groupId = 0; // dummy
+        props.history.push("/group-page/" + groupId);
+        break;
+      case NotificationTypes.FEEDBACK:
+        //TODO
+        break;
+      default:
+        break;
     }
+  };
 
-    return (
-        <Menu
-            open={props.open}
-            anchorEl={props.anchor}
-            onClose={props.onClose}
-            getContentAnchorEl={null}
-            anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right",
-            }}
-        >
-            {renderNotificationItems(fetchNotifications())}
-            <Divider key="divider" />
-            <MenuItem className={classes.menuitem}>
-                Mark as read
-            </MenuItem>
-        </Menu>
-    );
+  return (
+    <Menu
+      anchorEl={props.anchor}
+      open={props.open}
+      onClose={props.onClose}
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+    >
+      <ListItem key={"Noty"} button dense >
+        <ListItemText
+            primary={"Hobb.ee Meetup"}
+            secondary={"Hobb.ee is awesome!"}
+        />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <Divider key="divider" />
+      <ListItem button>
+        <Typography component="span" align="right" variant="body2">
+          Mark all as read
+        </Typography>
+      </ListItem>
+    </Menu>
+  );
 }
 
 // attributes of props and their type
 NotificationMenu.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    anchor: PropTypes.element,
-    open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  anchor: PropTypes.object,
+  open: PropTypes.bool.isRequired,
+  notifications: PropTypes.array.isRequired,
 };
 
 export default connect()(withRouter(NotificationMenu));
