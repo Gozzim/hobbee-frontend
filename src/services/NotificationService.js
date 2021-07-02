f5a577a179203212befde087d5fca916aac784c8
Kempec Halk <kempec.halk@tum.de>
2021-07-02 20:38:44 +0200

    fix(Service/Notification): Fix wrong import

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -1,4 +1,4 @@
-import HttpService from "./TestHttpService";
+import HttpService from "./HttpService";
 
 export async function fetchNotificationRequest(date) {
   const resp = await HttpService.get("/storage/notifications?since=" + date);
