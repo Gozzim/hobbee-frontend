f408f05bbcf3865a0ba407c60090c3d1e345fa1e
Kempec Halk <kempec.halk@tum.de>
2021-07-14 01:27:55 +0200

    refactor(Views/Structure): Rearrange some views into folders

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from "react";
-import UserLoginView from "./views/UserLoginView";
-import SignUpView from "./views/SignUpView";
+import SignInView from "./views/UserAuthentication/SignInView";
+import SignUpView from "./views/UserAuthentication/SignUpView";
 import { CreateGroupView } from "./views/CreateGroupView";
 import { HomeView } from "./views/HomeView";
 import { InMyAreaView } from "./views/InMyAreaView";
 import { PremiumView } from "./views/PremiumView";
 import { RecommendedView } from "./views/RecommendedView";
 import { MyGroupsView } from "./views/MyGroupsView";
-import { Imprint } from "./views/Imprint";
-import { PrivacyPolicy } from "./views/PrivacyPolicy";
-import { ToS } from "./views/TermsOfService";
+import { Imprint } from "./views/Legal/Imprint";
+import { PrivacyPolicy } from "./views/Legal/PrivacyPolicy";
+import { ToS } from "./views/Legal/TermsOfService";
 import { ProfileView } from "./views/ProfileView";
 import ResetPasswordView from "./views/UserAuthentication/ResetPasswordView";
 import { GroupPageView } from "./views/GroupPageView";
@@ -26,7 +26,7 @@ export const routes = [
   },
   {
     path: "/login",
-    component: UserLoginView,
+    component: SignInView,
   },
   {
     path: "/register",
