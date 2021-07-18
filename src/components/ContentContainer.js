b3545e8264257e5cb430786f01f7be870232af2b
Vanessa Krohn <vanessa.krohn@tum.de>
2021-07-18 02:20:51 +0200

    feat(ContentContainer): add advertisement banner

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
+import ad1 from "../assets/banner1.png";
+import { AdBanner } from "./AdBanner";
 
 const useStyles = makeStyles((theme) => ({
   contentContainerRoot: {
+    paddingRight: "300px",
     height: "calc(100vh - 64px)",
-    paddingTop: "40px",
+    display: "flex",
+    alignItems: "center",
   },
   wrapper: {
+    paddingTop: "40px",
     height: "100%",
     width: "100%",
     display: "flex",
+    overflow: "scroll",
     flexDirection: "column",
   },
   main: {
@@ -29,6 +35,7 @@ export function ContentContainer(props) {
 
   return (
     <div className={classes.contentContainerRoot}>
+      <AdBanner />
       <Container className={classes.wrapper} maxWidth="md">
         <main className={classes.main}>{children}</main>
         {footer}
