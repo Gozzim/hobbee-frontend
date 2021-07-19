480eb5d51e0f3b8558b50400eaf04101d8133ab2
Vanessa Krohn <vanessa.krohn@tum.de>
2021-07-19 19:27:40 +0200

    fix(ContentContainer): improve responsiveness

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
-import ad1 from "../assets/banner1.png";
 import { AdBanner } from "./AdBanner";
 
 const useStyles = makeStyles((theme) => ({
   contentContainerRoot: {
-    paddingRight: "300px",
     height: "calc(100vh - 64px)",
     display: "flex",
     alignItems: "center",
+  },
+  scrollContainer: {
+    display: "flex",
+    height: "100%",
+    width: "100%",
+    overflow: "scroll",
   },
   wrapper: {
     paddingTop: "40px",
     height: "100%",
     width: "100%",
     display: "flex",
-    overflow: "scroll",
     flexDirection: "column",
+    flexShrink: 0,
   },
   main: {
     flexGrow: 1,
+  },
+  spacer: {
+    width: "300px",
+    flexShrink: 1,
   },
 }));
 
@@ -36,10 +44,13 @@ export function ContentContainer(props) {
   return (
     <div className={classes.contentContainerRoot}>
       <AdBanner />
-      <Container className={classes.wrapper} maxWidth="md">
-        <main className={classes.main}>{children}</main>
-        {footer}
-      </Container>
+      <div className={classes.scrollContainer}>
+        <Container className={classes.wrapper} maxWidth="md">
+          <main className={classes.main}>{children}</main>
+          {footer}
+        </Container>
+        <div className={classes.spacer} />
+      </div>
     </div>
   );
 }
