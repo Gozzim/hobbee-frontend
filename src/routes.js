986c2aeae573686b525812b0ed51404c62b26859
Maja Schuknecht <maja.schuknecht@web.de>
2021-07-23 16:25:31 +0200

    fix(group-page): more visual fixes, group page completed

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,7 @@ import { GroupPageView } from "./views/GroupPageView";
 import { PaymentPlanView } from "./views/PremiumSubscription/PaymentPlanView";
 import { PaymentConfirmationView } from "./views/PremiumSubscription/PaymentConfirmationView";
 import { FeedbackView } from "./views/Feedback/FeedbackView";
+import { AccountSettingsView} from "./views/PremiumSubscription/AccountSettingsView";
 
 export const routes = [
   {
@@ -57,6 +58,11 @@ export const routes = [
     path: "/premium/payment-confirmation",
     Component: PaymentConfirmationView,
     label: "Confirmation",
+  },
+  {
+    path: "/account-settings",
+    Component: AccountSettingsView,
+    label: "Account Settings",
   },
   {
     path: "/recommended",
