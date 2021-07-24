78f500bcc15a387c690119a1cf86484111e2ca68
Vanessa Krohn <vanessa.krohn@tum.de>
2021-07-24 16:21:15 +0200

    feat(HomeView): add search bar, cleanup SearchBarComponent

--- src/views/InMyAreaView.js
+++ src/views/InMyAreaView.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { SearchBarComponent } from "../components/SeachBarComponent";
 import { useDispatch, useSelector } from "react-redux";
 import { getGroupsInMyArea } from "../redux/reducers/groupsReducer";
+import { SearchBar } from "../components/Search/SearchBar";
+import { SearchResults } from "../components/Search/SearchResults";
+import { useSearch } from "../hooks/useSearch";
 
 export function InMyAreaView() {
   const groups = useSelector((state) => {
     return state.groups.inMyArea.map((id) => state.groups.data[id]);
   });
+  const search = useSearch({ groups });
 
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
@@ -16,5 +19,10 @@ export function InMyAreaView() {
     }
   }, [user.isLoggedIn]);
 
-  return <SearchBarComponent groups={groups} title="GROUPS IN YOUR AREA" />;
+  return (
+    <>
+      <SearchBar search={search} />
+      <SearchResults search={search} title="GROUPS IN YOUR AREA" />
+    </>
+  );
 }
