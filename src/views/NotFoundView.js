92d80d63ec28a2d7bcf449142b1a3fed54c2a78c
Zaim Sari <zaim.sari@tum.de>
2021-07-25 17:03:49 +0200

    feat(404): added 404 page

---
+++ src/views/NotFoundView.js
@@ -0,0 +1,17 @@
+import React from "react";
+/**
+ * For my groups
+ * @param {props} props
+ */
+export function NotFoundView(props) {
+  return (
+    <center>
+      <h1>
+        You got lost!
+      </h1>
+      <h4>
+        404 - not found
+      </h4>
+    </center>
+  );
+}
